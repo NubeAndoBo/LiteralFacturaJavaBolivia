b22b1d805b7f403464eb22174d5f5048ea9bcd2c
acq1305 <angel@nubeando.com>
2017-01-19 11:04:04 -0400

    Metodo para transformar un numero en su literal

---
+++ src/main/java/com/nubeando/literalfacturajavabolivia/literalfacturajavabolivia/Run.java
@@ -0,0 +1,17 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.nubeando.literalfacturajavabolivia.literalfacturajavabolivia;
+
+/**
+ *
+ * @author acq1305
+ */
+public class Run {
+    public static void main(String[] args) {
+        Literal obj= new Literal();
+        System.out.println(Literal.toLiteral(1245687));
+    }
+}
